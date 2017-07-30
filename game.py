d17c581a778aa798cf2580e15b365483e8b8df77
Quade <qkirby@ufl.edu>
2017-07-30 17:41:37 -0400

    adding basic entities

--- game.py
+++ game.py
@@ -2,16 +2,19 @@ import pygame
 import time
 from time import sleep
 import glob
-from load_functions import reader
+from load_functions import reader, write
 
 pygame.init()
 
-
-
+# notes make teleport function and have the back ground move instead of character
+#most important finish intro for new game
+#add response function for responding to people
 info = []
 level = 0
-global_x = 150
-global_y = 180
+global_x = 154
+global_y = 170
+background_x = 0
+background_x = 0
 background = pygame.image.load("town.png")
 size = (width, height) = background.get_size()
 backgroundRect = background.get_rect()
@@ -42,6 +45,8 @@ def player(x =0,y = 0, tupl = [1, 0, 33, 33], count = 0):
     #time.sleep(2)
     #V needs work 285,210,510,325
 def obsticle(block,x,y):
+    global global_x
+    global global_y
     # dem constants have to do with the speed
     if (x-6) > block[0] and (x+6) < block[2]:
         if y > block[1] and y < block[3]:
@@ -64,14 +69,16 @@ def obsticle(block,x,y):
 
 
 
-def navigator(level=0, x=150,y=180):
+def navigator(level=0):
+    global global_x
+    global global_y
     if level == 0:
         intro()
 
     if level == 1:
-        stage1(x,y)
-    if level ==2:
-        hospital(x,y)
+        stage1(global_x,global_y)
+    if level == 2:
+        hospital()
 
     if level == 3:
         new_load()
@@ -79,18 +86,22 @@ def navigator(level=0, x=150,y=180):
         load_screen()
     if level == 5:
         Display_loads()
+    if level == 6:
+        Lab()
 
 
 
 
-def stage1(x=150,y=180):
+def stage1(x=151,y=250):
+    
     pygame.mixer.music.load('town.wav')
     background = pygame.image.load("town.png")
     #size = (width, height) = background.get_size()
     #backgroundRect = background.get_rect()
     #menu = pygame.image.load("PauseMenu.png")
-
-    
+    global global_x
+    global global_y
+    global level
     
     
     background = pygame.transform.scale(background,(942,567))
@@ -113,7 +124,7 @@ def stage1(x=150,y=180):
     
 
     gameExit = False
-    pygame.mixer.music.play(-1)
+    #pygame.mixer.music.play(-1)
     while not gameExit:
 
         for event in pygame.event.get():
@@ -143,6 +154,8 @@ def stage1(x=150,y=180):
                 if event.key == pygame.K_q:
                     print "yup"
                     pausemenu()
+                    x_change = 0
+                    y_change = 0
                 
 
             elif event.type == pygame.KEYUP:
@@ -156,61 +169,66 @@ def stage1(x=150,y=180):
                 
 
 
-        x += x_change
-        y += y_change
+        global_x += x_change
+        global_y += y_change
         count += countc
 
-        #gameDisplay.fill(white)
-
-        
-        
         gameDisplay.blit(background,hombreRect)
-        #gameDisplay.blit(menu,(-325,10))
-        #message_display("tester")
         
-        #car(500,-500)
-        print "x: %f" %x
-        print "y: %f" %y
+        
+        print "x: %f" %global_x
+        print "y: %f" %global_y
         #basic borders
         
-        if x < 36:
-            x += 5
-        if x > 840:
-            x = 840
-        if y > 517:
-            y = 517
-        if y < 30:
-            y = 30
+        if global_x < 36:
+            global_x += 5
+        if global_x > 840:
+            global_x = 840
+        if global_y > 517:
+            global_y = 517
+        if global_y < 30:
+            global_y = 30
             #buildings
-        x,y = obsticle([285,210,510,325],x,y)
-        x,y = obsticle([490,0,840,190],x,y)
-        x,y = obsticle([90,350,510,460],x,y)
-        x,y = obsticle([140,25,345,140],x,y)
-        x,y = obsticle([680,315,900,440],x,y)
+            
+        global_x,global_y = obsticle([285,210,510,325],global_x,global_y)
+        global_x,global_y = obsticle([490,0,840,190],global_x,global_y)
+        global_x,global_y = obsticle([90,350,510,460],global_x,global_y)
+        global_x,global_y = obsticle([140,25,345,140],global_x,global_y)
+        global_x,global_y = obsticle([680,315,900,440],global_x,global_y)
+        
         #walking animation
         if count > 19:
             count = 0
         
-        player(x,y,tupl,count)
-        #pokemon entrance
-        if x >230 and x < 255:
-            if y < 145 and y > (145 - 50):
+        player(global_x,global_y,tupl,count)
+        #hospital entrance
+        pygame.display.update()
+        if global_x >230 and global_x < 255:
+            if global_y < 145 and global_y > (145 - 50):
                 gameExit = True
                 portal_val = 2
-                portal_x = 460
+                portal_x = 457
                 portal_y = 470
+        
+        if global_x >385 and global_x < 405:
+            if global_y < 326 and global_y > (326 - 50):
+                gameExit = True
+                portal_val = 6
+                portal_x = 457
+                portal_y = 470
+                
 
             
         
         
-        pygame.display.update()
+        
         clock.tick(30)
     pygame.mixer.music.fadeout(1500)
-    global level
+    
     level = portal_val
-    global global_x
+    
     global_x = portal_x
-    global global_y
+    
     global_y = portal_y
     
 
@@ -261,7 +279,12 @@ def intro():
     level = 4
     
 
-def hospital(x=150,y=180):
+def hospital():
+    global global_x
+    
+    global level
+    global global_y
+    
     background = pygame.image.load("hospital.png")
     background = pygame.transform.scale(background,(942,567))
     #size = (width, height) = background.get_size()
@@ -318,8 +341,10 @@ def hospital(x=150,y=180):
                     countc += 1
 
                 if event.key == pygame.K_q:
-                    print "yup"
                     pausemenu()
+                    x_change = 0
+                    y_change = 0
+                    
                     
                 
 
@@ -330,11 +355,12 @@ def hospital(x=150,y=180):
                 if event.key == pygame.K_DOWN or event.key == pygame.K_UP:
                     y_change = 0
                 countc = 0
+                count = 0
                 
 
 
-        x += x_change
-        y += y_change
+        global_x += x_change
+        global_y += y_change
         count += countc
 
         #gameDisplay.fill(white)
@@ -345,44 +371,41 @@ def hospital(x=150,y=180):
         #message_display("tester")
         
         #car(500,-500)
-        print "x: %f" %x
-        print "y: %f" %y
+        print "x: %f" %global_x
+        print "y: %f" %global_y
         #basic borders
         
-        if x < 36:
-            x += 5
-        if x > 840:
-            x = 840
-        if y > 517:
-            y = 517
-        if y < 30:
-            y = 30
+        if global_x < 36:
+            global_x += 5
+        if global_x > 840:
+            global_x = 840
+        if global_y > 517:
+            global_y = 517
+        if global_y < 30:
+            global_y = 30
             '''
         x,y = obsticle([285,210,510,325],x,y)
         x,y = obsticle([490,0,840,190],x,y)
         x,y = obsticle([90,350,510,460],x,y)
         '''
-        x,y = obsticle([260,115,655,235],x,y)
+        global_x,global_y = obsticle([260,115,655,235],global_x,global_y)
         if count > 19:
             count = 0
         
-        player(x,y,tupl,count)
+        player(global_x,global_y,tupl,count)
 
-        # the exit
-        if x > 410 and x < 510:
-            if y > 500:
+        # the eglobal_xit
+        if global_x > 410 and global_x < 510:
+            if global_y > 500:
                 gameExit = True
-
-            
-        #if x 
         
         pygame.display.update()
         clock.tick(30)
-    global level
+    
     level = 1
-    global global_x
+    
     global_x = 240
-    global global_y
+    
     global_y = 145
 
     
@@ -424,22 +447,55 @@ def pausemenu():
 
                 if event.key == pygame.K_UP:
                     location -= 1
+                if event.key == pygame.K_RETURN:
+                    MenuOption(location)
+                    return
                    
         if location < 1:
             location = 1
         if location > 9:
             location = 9;
 
+         
+
+def MenuOption(value):
+    if value == 4:
+        result = display_option(["yes","no","sure","mabye?"])
+        if result == 1:
+            display_text(["you fucked up man."])
+    if value == 5:
+        display_text(["Your name is " + info[1]])
+    if value == 6:
+        #save
+        #global info
+        global global_x
+        global global_y
+        global level
+        info[3] = global_x
+        info[4] = global_y
+        info[2] = level
+        write(info)
+
+        display_text(["The Game has been saved."])
+
 def new_load():
     gameDisplay.fill((0, 0, 0))
     text_box = pygame.image.load("text_box.png")
-    prof = pygame.image.load("pixel_moore.png")
+    global info
+    global level
+    level = 1
+    global global_x
+    global global_y
+
+    info.append("from_start.txt") 
+
+    
     gameExit = False
     name = ''
     font = pygame.font.Font("font.ttf", 15)
-    gameDisplay.blit(prof,(10,250))
-    display_text(["Welcome to the University of Florida","More importantly Welcome  to our Department"])
-
+    #gameDisplay.blit(prof,(10,250))
+    display_text(["Welcome to the University of Florida","More importantly Welcome  to our Department","of Electrical and         Computer Engineering"],1)
+    display_text(["at your stay here you     will be seeing A lot","well tell me your name kid"],1)
     while not gameExit:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -452,28 +508,35 @@ def new_load():
                 if event.key == pygame.K_BACKSPACE:
                     name = name[:-1]
                 if event.key == pygame.K_RETURN:
-                    name = ""
+                    gameExit = True
+                    info.append(name)
                 if event.key == pygame.K_SPACE:
                     name += " "
                     
-                        
-
-            gameDisplay.fill((0, 0, 0))
+           
             block = font.render(name, True, (0, 0, 0))
             rect = block.get_rect()
-            #rect.center = gameDisplay.get_rect().center
             
             gameDisplay.blit(text_box,(10,400))
             gameDisplay.blit(block, (30,425))
                     
             pygame.display.update()
             clock.tick(30)
+    info.append(1)
+    info.append(global_x)
+    info.append(global_y)
+    #health
+    info.append(4)
+    #skills
+    info.append("[]")
+    #
 
                 
-def display_text(phrase = ["empty"],speed = .09,x = 10, y = 400):
+def display_text(phrase = ["empty"],character = 0,speed = .07,x = 10, y = 400):
     text_box = pygame.image.load("text_box.png")
-    
-    
+    if character == 1:
+        prof = pygame.image.load("pixel_moore.png")
+        gameDisplay.blit(prof,(x,y-150))
     font = pygame.font.Font("font.ttf", 15)
     
     #important to keep sentence under certain  ###27###
@@ -493,8 +556,10 @@ def display_text(phrase = ["empty"],speed = .09,x = 10, y = 400):
             pygame.display.update()
             clock.tick(30)                
             sleep(speed)
-
-
+        for event in pygame.event.get():
+                if event.type == pygame.K_RETURN:
+                    pygame.event.clear(pygame.K_RETURN)
+                    
         while not gameExit:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -510,10 +575,14 @@ def display_text(phrase = ["empty"],speed = .09,x = 10, y = 400):
         pygame.display.update()
 
 def load_screen():
-    gameDisplay.fill((128,128,128))
+    
     font = pygame.font.Font("font.ttf", 15)
     menu = pygame.image.load("text_box.png")
     point = pygame.image.load('menupointer.png')
+    background = pygame.image.load('loadback.png')
+
+    background = pygame.transform.scale(background,(942,567))
+    gameDisplay.blit(background,[0,0])
     block1 = font.render("Load save", True, (0, 0, 0))
     block2 = font.render("New Game", True, (0, 0, 0))
     location = 1
@@ -563,7 +632,11 @@ def load_screen():
 def Display_loads():
     possible = glob.glob("*.txt")
     gameExit = False
-    gameDisplay.fill((128,128,128))
+    gameDisplay.fill((0,0,0))
+    background = pygame.image.load('loadback.png')
+
+    background = pygame.transform.scale(background,(942,567))
+    gameDisplay.blit(background,[0,0])
     font = pygame.font.Font("font.ttf", 15)
     point = pygame.image.load('menupointer.png')
     menu = pygame.image.load('LoadMenu.png')
@@ -608,10 +681,281 @@ def Display_loads():
     global level
     global global_x
     global global_y
+    
     info = reader(possible[location-1])
-    level = int(info[4])
-    global_x = int(info[5])
-    global_y = int(info[6])
+
+    
+    level = int(info[2])
+    global_x = int(info[3])
+    global_y = int(info[4])
+
+def display_option(options):
+    
+    gameExit = False
+    point = pygame.image.load('menupointer.png')
+    location = 1
+    font = pygame.font.Font("font.ttf", 15)
+    if len(options) == 2:
+        menu = pygame.image.load('text_box.png')
+        x = 10
+        y = 450
+        pointy_offset = 420
+        wordx_offset = 50
+        wordy_offset = 0
+
+
+    if len(options) >= 3:
+        x = -100
+        y = 300
+        pointy_offset = 375
+        wordx_offset = 175
+        wordy_offset = 100
+        menu = pygame.image.load('LoadMenu.png')
+
+
+    while not gameExit:
+        i = 1
+        #gameDisplay.blit(menu,(250,10))
+        gameDisplay.blit(point,(-325,location*32 + pointy_offset ))
+        
+        pygame.display.update()
+        gameDisplay.blit(menu,(x,y))
+
+        for option in options:
+            block = font.render(option, True, (0, 0, 0))
+            gameDisplay.blit(block, (x+ wordx_offset,y + i*32 + wordy_offset))
+            i +=1
+
+        
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+            if event.type == pygame.KEYDOWN:
+
+
+                if event.key == pygame.K_DOWN:
+                    location += 1
+
+                if event.key == pygame.K_UP:
+                    location -= 1
+                if event.key == pygame.K_RETURN:
+                    gameExit = True
+
+        if location < 1:
+            location = 1
+        if location > len(options):
+            location = len(options)
+
+    return location
+
+def battle(foe = 0):
+
+
+
+    while not gameExit:
+        i = 1
+        #gameDisplay.blit(menu,(250,10))
+        gameDisplay.blit(point,(-325,location*32 + pointy_offset ))
+        
+        pygame.display.update()
+        
+
+        
+
+        
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+def Lab():
+    global global_x
+    
+    global level
+    global global_y
+    
+    background = pygame.image.load("lab.png")
+    background = pygame.transform.scale(background,(942,567))
+    #size = (width, height) = background.get_size()
+    #backgroundRect = background.get_rect()
+
+    
+    hombreRect = background.get_rect()
+    #size1 = (width, height) = hombre.get_size()
+    
+
+    #gameDisplay = pygame.display.set_mode(size1)
+
+    tupl = [33, 0, 33, 33]
+
+
+
+
+    
+
+    x_change = 0
+    y_change = 0
+    count = 0
+    countc = 0
+
+    
+
+    gameExit = False
+
+    while not gameExit:
+
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x_change = -5
+                    tupl = [1, 33, 33, 30]
+                    countc += 1
+                elif event.key == pygame.K_RIGHT:
+                    x_change = 5
+                    tupl = [1, 64, 33, 32]
+                    countc += 1
+
+                elif event.key == pygame.K_UP:
+                    y_change = -5
+                    tupl = [1, 96, 31, 33]
+                    countc += 1
+
+                elif event.key == pygame.K_DOWN:
+                    y_change = 5
+                    tupl = [1, 0, 31, 33]
+                    countc += 1
+
+                elif event.key == pygame.K_q:
+                    pausemenu()
+                    print "yup"
+                    x_change = 0
+                    y_change = 0
+                    
+            
+
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    print "key are up"
+                    x_change = 0
+
+                if event.key == pygame.K_DOWN or event.key == pygame.K_UP:
+                    y_change = 0
+                countc = 0
+                count = 0
+        gameDisplay.blit(background,hombreRect)
+                
+        x_change,y_change = character(x_change,y_change,250,250,["why are you disappearing?"])
+
+        global_x += x_change
+        global_y += y_change
+        count += countc
+
+        #gameDisplay.fill(white)
+
+        
+        
+        
+        #message_display("tester")
+        
+        #car(500,-500)
+        print "x: %f" %global_x
+        print "y: %f" %global_y
+        #basic borders
+        
+        if global_x < 36:
+            global_x += 5
+        if global_x > 840:
+            global_x = 840
+        if global_y > 517:
+            global_y = 517
+        if global_y < 30:
+            global_y = 30
+            '''
+        x,y = obsticle([285,210,510,325],x,y)
+        x,y = obsticle([490,0,840,190],x,y)
+        x,y = obsticle([90,350,510,460],x,y)
+        '''
+        
+        if count > 19:
+            count = 0
+        
+        
+        player(global_x,global_y,tupl,count)
+        pygame.display.update()
+        
+
+
+        # the eglobal_xit
+        if global_x > 410 and global_x < 510:
+            if global_y > 500:
+                gameExit = True
+        
+        
+        clock.tick(30)
+    
+    level = 1
+    global_x = 395
+    global_y = 335
+
+
+
+
+def character(m,v,locationx = 250,locationy = 250,statement = ["..."],person = 1):
+    global global_x
+    global global_y
+    x = global_x
+    y = global_y
+    block = [locationx-20,locationy-20,locationx + 30,locationy + 30]
+    npc = pygame.image.load("red.png")
+    if person == 0:
+        tupl = [0, 0, 0, 0]
+    if person == 1:
+        tupl = [1, 0, 33, 33]
+
+    gameDisplay.blit(npc,(locationx,locationy),tupl)
+    if (x-6) > block[0] and (x+6) < block[2]:
+        if y > block[1] and y < block[3]:
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RETURN:
+                        display_text(statement)
+            if -6 <= (y-block[3]): # has to be slightly greater than moving speed
+                y = block[3] + 1
+            else: y = block[1] -1
+            m,v = 0,0
+
+            
+        
+
+    if y > block[1] and y < block[3]:
+        if x > block[0] and x < block[2]:
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RETURN:
+                        display_text(statement)
+            if -6 <= (x-block[2]):
+                x= block[2] + 1
+            else: x = block[0] - 1
+            m,v =  0,0
+    global_x = x
+    global_y = y
+    return m,v
+    
+
+
+
+
+
+
 
 
 
@@ -643,21 +987,8 @@ def Display_loads():
         
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 while True:
 
-    navigator(level,global_x,global_y)
+    navigator(level)
 pygame.quit()
 quit()
